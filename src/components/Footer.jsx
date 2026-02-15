65e700fed4d6af070fac836aaf48578f9a9b5b42
EslamRage7 <eslamrageh41@gmail.com>
2026-02-15 17:32:44 +0200

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,19 @@
 import { useEffect } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
+import {
+  FaUserTie,
+  FaLink,
+  FaBriefcase,
+  FaHome,
+  FaAddressCard,
+  FaAward,
+  FaEnvelope,
+  FaBook,
+  FaFileInvoiceDollar,
+  FaSearchDollar,
+  FaChartLine,
+} from "react-icons/fa";
 import "./css/Footer.css";
 import { useSiteContent } from "../data/SiteContentContext";
 
@@ -8,6 +21,18 @@ export default function Footer() {
   const currentYear = new Date().getFullYear();
   const { content } = useSiteContent();
   const { footer } = content;
+  const linkIcons = {
+    Home: <FaHome />,
+    About: <FaAddressCard />,
+    Certificates: <FaAward />,
+    Contact: <FaEnvelope />,
+  };
+  const serviceIcons = {
+    Bookkeeping: <FaBook />,
+    "Tax Filing": <FaFileInvoiceDollar />,
+    "Audit Support": <FaSearchDollar />,
+    Reporting: <FaChartLine />,
+  };
 
   useEffect(() => {
     AOS.init({ duration: 900, once: false });
@@ -20,7 +45,12 @@ export default function Footer() {
           <div className="row g-4 footer-grid">
             <div className="col-lg-4 col-md-4" data-aos="fade-up">
               <div className="footer-col">
-                <h5 className="footer-title">{footer.title}</h5>
+                <h5 className="footer-title">
+                  <span className="footer-title-icon">
+                    <FaUserTie />
+                  </span>
+                  {footer.title}
+                </h5>
                 <p className="footer-text">{footer.text}</p>
               </div>
             </div>
@@ -29,11 +59,19 @@ export default function Footer() {
               data-aos="fade-up"
               data-aos-delay="80">
               <div className="footer-col">
-                <h5 className="footer-title">{footer.linksTitle}</h5>
+                <h5 className="footer-title">
+                  <span className="footer-title-icon">
+                    <FaLink />
+                  </span>
+                  {footer.linksTitle}
+                </h5>
                 <ul className="footer-links">
                   {footer.links.map((link, index) => (
                     <li key={`footer-link-${index}`}>
-                      <a href={link.href}>{link.label}</a>
+                      <a href={link.href}>
+                        <span className="footer-link-icon">{linkIcons[link.label] || <FaLink />}</span>
+                        {link.label}
+                      </a>
                     </li>
                   ))}
                 </ul>
@@ -44,11 +82,21 @@ export default function Footer() {
               data-aos="fade-up"
               data-aos-delay="160">
               <div className="footer-col">
-                <h5 className="footer-title">{footer.servicesTitle}</h5>
+                <h5 className="footer-title">
+                  <span className="footer-title-icon">
+                    <FaBriefcase />
+                  </span>
+                  {footer.servicesTitle}
+                </h5>
                 <ul className="footer-links">
                   {footer.services.map((service, index) => (
                     <li key={`footer-service-${index}`}>
-                      <a href={service.href}>{service.label}</a>
+                      <a href={service.href}>
+                        <span className="footer-link-icon">
+                          {serviceIcons[service.label] || <FaBriefcase />}
+                        </span>
+                        {service.label}
+                      </a>
                     </li>
                   ))}
                 </ul>
@@ -57,6 +105,9 @@ export default function Footer() {
           </div>
 
           <hr className="footer-divider" />
+          <p className="footer-bottom">
+            {currentYear} {footer.title}. All rights reserved.
+          </p>
         </div>
       </div>
     </footer>
