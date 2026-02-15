84dd389a856cdea5c20897297d2001ee63a07bec
EslamRage7 <eslamrageh41@gmail.com>
2026-02-15 17:28:56 +0200

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -17,30 +17,42 @@ export default function Footer() {
     <footer className="footer">
       <div className="footer-content">
         <div className="container py-4">
-          <div className="row gy-4">
-            <div className="col-md-4" data-aos="fade-up">
-              <h5 className="footer-title">{footer.title}</h5>
-              <p className="footer-text">{footer.text}</p>
+          <div className="row g-4 footer-grid">
+            <div className="col-lg-4 col-md-4" data-aos="fade-up">
+              <div className="footer-col">
+                <h5 className="footer-title">{footer.title}</h5>
+                <p className="footer-text">{footer.text}</p>
+              </div>
             </div>
-            <div className="col-md-4" data-aos="fade-up" data-aos-delay="80">
-              <h5 className="footer-title">{footer.linksTitle}</h5>
-              <ul className="footer-links">
-                {footer.links.map((link, index) => (
-                  <li key={`footer-link-${index}`}>
-                    <a href={link.href}>{link.label}</a>
-                  </li>
-                ))}
-              </ul>
+            <div
+              className="col-lg-4 col-md-4"
+              data-aos="fade-up"
+              data-aos-delay="80">
+              <div className="footer-col">
+                <h5 className="footer-title">{footer.linksTitle}</h5>
+                <ul className="footer-links">
+                  {footer.links.map((link, index) => (
+                    <li key={`footer-link-${index}`}>
+                      <a href={link.href}>{link.label}</a>
+                    </li>
+                  ))}
+                </ul>
+              </div>
             </div>
-            <div className="col-md-4" data-aos="fade-up" data-aos-delay="160">
-              <h5 className="footer-title">{footer.servicesTitle}</h5>
-              <ul className="footer-links">
-                {footer.services.map((service, index) => (
-                  <li key={`footer-service-${index}`}>
-                    <a href={service.href}>{service.label}</a>
-                  </li>
-                ))}
-              </ul>
+            <div
+              className="col-lg-4 col-md-4"
+              data-aos="fade-up"
+              data-aos-delay="160">
+              <div className="footer-col">
+                <h5 className="footer-title">{footer.servicesTitle}</h5>
+                <ul className="footer-links">
+                  {footer.services.map((service, index) => (
+                    <li key={`footer-service-${index}`}>
+                      <a href={service.href}>{service.label}</a>
+                    </li>
+                  ))}
+                </ul>
+              </div>
             </div>
           </div>
 
